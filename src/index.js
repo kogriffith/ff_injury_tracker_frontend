ffe73adcd56516a18df5d61a2cb31e20894ad266
kogriffith <kevin.o.griffith@gmail.com>
2020-11-16 23:22:39 -0800

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom'
+import App from './components/App';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { createHttpLink } from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import './styles/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+import * as serviceWorker from './serviceWorker';
+
+const link = createHttpLink({
+  uri: 'localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache() 
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
