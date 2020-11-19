1e658da68f6a1d1fd8005ece148a891e47f9e5c7
kogriffith <kevin.o.griffith@gmail.com>
2020-11-18 21:43:01 -0800

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Link, 
+        Switch,
+        Route,
+        useParams,
+        Redirect} from 'react-router-dom'
+import {Container} from 'reactstrap'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CustomNavbar></CustomNavbar>
+        <Container>
+        <Switch>
+          <Redirect exact from="/" to= "/home" />
+          <Route path = "/home">
+            <Home/>
+          </Route>
+          <Route exact path = "/players/:id">
+            <PlayerCard></PlayerCard>
+          </Route>
+        </Switch>
+        </Container>
     </div>
   );
 }
